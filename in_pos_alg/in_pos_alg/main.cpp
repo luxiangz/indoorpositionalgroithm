371a63c6663f22c8d1e75928725c5f3beb19f88e
luxiangz <luxiangz@mail.ustc.edu.cn>
2017-09-18 08:54:25 +0800

    主函数文件中写入数据不能保存

--- in_pos_alg/in_pos_alg/main.cpp
+++ in_pos_alg/in_pos_alg/main.cpp
@@ -6,65 +6,119 @@
 
 #include "file_operation.h"
 using namespace std;
+struct potcollection
+{
+	int firstp;
+	int secondp;
+	int thirdp;
+	int fourthp;
+};
+struct coordinatedata
+{
+public:
+	float x = {};
+	float y = {};
+	float z = {};
+	/*potcollection *index = {};
+	coordinatedata(int pointnum0, int formsurface0)
+	{
+		float *x = new float[pointnum0];
+		float *y = new float[pointnum0];
+		float *z = new float[pointnum0];
+		potcollection *index = new potcollection[pointnum0];
+		cout << "struct构造函数" << endl;
+	}
+	~coordinatedata()
+	{
+		delete[] x;
+		delete[] y;
+		delete[] z;
+		delete[] index;
+		cout << "struct析构函数" << endl;
+	}*/
+
+};
+int formsurface[] = {
+4,5,6,1,
+5,8,7,6,
+8,14,11,7,
+16,14,10,11,
+15,16,11,12,
+13,15,12,9,
+3,13,9,2,
+3,4,1,2,
+21,22,19,20,
+24,21,20,17,
+24,23,18,17,
+22,23,18,19,
+20,19,18,14
+};
+#define pointnum 24
+float xtemp[pointnum] = { 000.0f, 000.0f, 000.0f, 000.0f, 491.5f, 491.5f, 491.5f, 491.5f, 089.2f, 108.3f, 108.3f, 089.2f, 089.2f, 108.3f, 089.2f, 108.3f, 200.0f, 350.0f, 350.0f, 200.0f, 200.0f, 350.0f, 350.0f, 200.0f };
+float ytemp[pointnum] = { 000.0f, 483.7f, 483.7f, 000.0f, 000.0f, 000.0f, 483.7f, 483.7f, 483.7f, 483.7f, 458.1f, 458.1f, 483.7f, 483.7f, 458.1f, 458.1f, 260.0f, 260.0f, 200.0f, 200.0f, 200.0f, 200.0f, 260.0f, 260.0f };
+float ztemp[pointnum] = { 200.0f, 200.0f, 000.0f, 000.0f, 000.0f, 200.0f, 200.0f, 000.0f, 200.0f, 200.0f, 200.0f, 200.0f, 000.0f, 000.0f, 000.0f, 000.0f, 050.0f, 050.0f, 050.0f, 050.0f, 000.0f, 000.0f, 000.0f, 000.0f };
+
+int Write_Into_Txt(char* filename, bool writeflag);
+
 int main()
 {
-	//int space_wall_number=0;//the number of walls in space
-	//cout << "The number of walls in space:"<<endl;
-	//cin >> space_wall_number;
 	char *filename = "./data.txt";
-	fstream datafile;//file include the datas of space
-	datafile.open("./data.txt", ios::in | ios::out);
-	if (!datafile)
+	bool writeflag = true;
+	if (Write_Into_Txt(filename, writeflag) == 0)
 	{
-		cerr << "Error opening file!" << endl;
 		return -1;
 	}
-	if (!datafile.fail())
-	{	
-		/*datafile << "This is a line.\n";
-		datafile << "This is another line.\n";*/
-
-		//write the alphabet in the file
-		char a = 'a';
-		int i;
-		for (i = 0; i < 26; i++)
-		{
-			datafile << setw(2) << i << "\t" << a << "\n";
-			a++;
-		}
-
-		int maxrow = CountDataRow(filename);
-		float *tempcoor = new float[maxrow];
-		char *tempcoorchar = new char[maxrow];
-
-		//test find the special row
-		//cout << maxrow << endl;
-		//string filerow_con = ReadDataRow(filename, 2);
-		//cout << filerow_con.c_str() << endl;
-		datafile.seekp(ios::beg);//move the pointer to the header
-		int i0 = 0;
-		while (!datafile.eof())
-		{
-			datafile >> tempcoor[i0];
-			datafile >> tempcoorchar[i0];
-			i0++;
-		}
-		datafile.close();	
-		for (i0 = 0; i0 < maxrow; i0++)
-			cout << tempcoor[i0] << "\t" << tempcoorchar[i0] << "\n";
-
-		delete[]tempcoor;
-		delete[]tempcoorchar;
-	}
-
-	
-	//while (!datafile.eof())
+	fstream datafile;
+	datafile.open(filename, ios::out);
+	if (!datafile)
 	{
-		//datafile.getline()
+		//cerr<<
 	}
+	datafile.close();
 	system("pause");
 	
 	return 0; 
 
 }
+int Write_Into_Txt(char* filename, bool writeflag)
+{
+	fstream datafile0;
+	int surfacenum = sizeof(formsurface) / sizeof(formsurface[0]) / 4;
+	cout << "surfacenum" << surfacenum << endl;
+	coordinatedata datac[pointnum];
+	
+	if (writeflag)
+	{
+		datafile0.open(filename, ios::in | ios::out);
+		if (!datafile0)
+		{
+			cerr << "Can't open the file!" << endl;
+			return 0;
+		}
+		if (!datafile0.fail())
+		{//select the four point clockwise to form a face
+			for (int i = 0; i < pointnum; i++)
+			{
+				datac[i].x = xtemp[i];
+				datac[i].y = ytemp[i];
+				datac[i].z = ztemp[i];
+				//cout << "data[" << i << "]\t" << datac[i].x << endl;
+				//datafile << setw(2) << datac[i].x << "\t" << datac[i].y << "\t" << datac[i].z << "\n";
+			}
+			for (int j = 0; j < surfacenum; j++)
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					int tempindex = formsurface[j * 4 + k];
+					//datafile0 << setw(2) << datac[tempindex - 1].x << "\t" << datac[tempindex - 1].y << "\t" << datac[tempindex - 1].z << "\t";
+				}
+				//datafile0 << "\n";
+			}
+			//cout << datac[9].x << "\t" << datac[9].y << "\t" << datac[9].z << "\n";
 
+			datafile0.close();
+		}
+	}
+	return 1;
+
+}
